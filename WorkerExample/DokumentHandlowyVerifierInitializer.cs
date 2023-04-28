34db716e46186c3577689b6aa9a279529c082efb
aplikacje <aplikacje@aprosystem.pl>
2023-04-28 16:10:22 +0200

    Problem z weryfikatorem

---
+++ WorkerExample/DokumentHandlowyVerifierInitializer.cs
@@ -0,0 +1,19 @@
+﻿using CisWorkery;
+using Soneta.Business;
+using Soneta.Handel;
+using WorkerExample;
+
+[assembly: ProgramInitializer(typeof(DokumentHandlowyVerifierInitializer))]
+namespace CisWorkery
+{
+    public class DokumentHandlowyVerifierInitializer : IProgramInitializer
+    {
+        public void Initialize()
+        {
+            HandelModule.DokumentHandlowySchema.AddOnEditing((Row) =>
+            {
+                Row.Session.Verifiers.Add(new WeryfikatorDokumentHandlowy(Row));
+            });
+        }
+    }
+}
---
+++ WorkerExample/WeryfikatorDokumentHandlowy.cs
@@ -0,0 +1,23 @@
+﻿using Soneta.Business;
+using Soneta.Handel;
+using Soneta.Types;
+using System.Linq;
+
+namespace WorkerExample
+{
+    public class WeryfikatorDokumentHandlowy : RowVerifier
+    {
+        public WeryfikatorDokumentHandlowy(Row dokHandlowy) : base(dokHandlowy) { }
+
+        private new DokumentHandlowy Row => (DokumentHandlowy)base.Row;
+
+        public override string Description => "Ilośc produktów przekroczona!";
+
+        public override VerifierType Type => VerifierType.Error;
+
+        protected override bool IsValid()
+        {
+            return Row.Pozycje.Sum(p => p.Ilosc.Value) <= 100;
+        }
+    }
+}
